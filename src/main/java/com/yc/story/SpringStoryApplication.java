5e5e99c25fd16c89e2a4ba935653f3929ae3fd8d
jiaowoToniwz <54107365+jiaowoToniwz@users.noreply.github.com>
2019-08-25 11:28:59 +0800

    ccc

--- src/main/java/com/yc/story/SpringStoryApplication.java
+++ src/main/java/com/yc/story/SpringStoryApplication.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Controller
 @MapperScan("com.yc")
 public class SpringStoryApplication implements WebMvcConfigurer {
-	
+	// llllll
 	//嘎
 	//lhr
 	//sad
